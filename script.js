523993235d712cf97e66d76c839d3e16404440f6
Bridgetamana <amanabridget03@gmail.com>
2025-11-25 18:30:14 +0100

    update CSS hover class

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let taskArray = [];
-const inputTask = document.getElementById("task")
-const addTaskBtn = document.getElementById("add-btn")
-const taskList = document.getElementById("task-list")
-const noTask = document.getElementById("no-task")
-const showQueue = document.getElementById("show-queue")
+const taskInput = document.getElementById("task")
+const addTaskBtn = document.getElementById("add-task-btn")
+const currentTask = document.getElementById("current-task")
+const emptyState = document.getElementById("empty-state")
+const showQueueBtn = document.getElementById("show-queue")
 const overlay = document.querySelector(".overlay")
-const hideQueue = document.querySelector(".hide-queue")
+const closeQueueBtn = document.querySelector(".close-queue-btn")
 loadTask()
 
 addTaskBtn.addEventListener("click", function () {
-    if (inputTask.value !== "") {
-        taskArray.push({ "status": "pending", "text": inputTask.value })
+    if (taskInput.value !== "") {
+        taskArray.push({ "status": "pending", "text": taskInput.value })
         displayTask()
         saveTask(taskArray)
-        inputTask.value = ""
+        taskInput.value = ""
     } else {
         alert("Add a task!")
     }
@@ -21,26 +21,26 @@ addTaskBtn.addEventListener("click", function () {
 
 function displayTask() {
     if (taskArray.length === 0) {
-        noTask.style.display = "block"
-        taskList.innerHTML = ""
+        emptyState.style.display = "block"
+        currentTask.innerHTML = ""
         return
     }
 
-    noTask.style.display = "none"
+    emptyState.style.display = "none"
 
     const li = document.createElement("li")
     li.classList.add("task-item")
     li.innerHTML = `
         <label>${taskArray[0].text}</label>
-        <button class="complete-task"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#dfd7d7ff" fill="none" stroke="#dfd7d7ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+        <button class="complete-task-btn hover"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#dfd7d7ff" fill="none" stroke="#dfd7d7ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
             <path d="M5 14.5C5 14.5 6.5 14.5 8.5 18C8.5 18 14.0588 8.83333 19 7" />
         </svg></button>
     `
 
-    taskList.innerHTML = ""
-    taskList.appendChild(li)
+    currentTask.innerHTML = ""
+    currentTask.appendChild(li)
 
-    li.querySelector(".complete-task").addEventListener("click", function () {
+    li.querySelector(".complete-task-btn").addEventListener("click", function () {
         taskArray.shift()
         displayTask()
         saveTask(taskArray)
@@ -57,10 +57,10 @@ function loadTask() {
     displayTask()
 }
 
-showQueue.addEventListener("click", () => {
+showQueueBtn.addEventListener("click", () => {
     overlay.style.display = "flex"
 })
 
-hideQueue.addEventListener("click", () => {
+closeQueueBtn.addEventListener("click", () => {
     overlay.style.display = "none"
 })
