97cad561e4cf7b978beb6089dad479ecdb3dfac3
Bridgetamana <amanabridget03@gmail.com>
2025-11-25 14:25:56 +0100

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let taskArray = [];
 const inputTask = document.getElementById("task")
 const addTaskBtn = document.getElementById("add-btn")
 const taskList = document.getElementById("task-list")
+const noTask = document.getElementById("no-task")
 loadTask()
 
 addTaskBtn.addEventListener("click", function () {
@@ -16,14 +17,19 @@ addTaskBtn.addEventListener("click", function () {
 })
 
 function displayTask() {
+    if (taskArray.length !== 0) {
+        noTask.style.display = "none"
+    }
     let displayedTask = ""
     taskArray.forEach((task, index) => {
         displayedTask += `
-        <li id="task${index}">
-            <input type="checkbox" name="user-task" id="user-task-${index}"/>
+            <li id="task${index}" class="task-item">
             <label for="user-task-${index}"> ${task.text} </label>
-        </li>
-        `
+            <button id="user-task-${index}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#dfd7d7ff" fill="none" stroke="#dfd7d7ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M5 14.5C5 14.5 6.5 14.5 8.5 18C8.5 18 14.0588 8.83333 19 7" />
+</svg></button>
+            </li>
+            `
     })
     taskList.innerHTML = displayedTask
 
